2a65ff79100e8d31f0163ccf770211c54eb0064a
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2024-02-15 18:51:39 +0300

    upd: whywhy app refinement

--- whywhy/flow.py
+++ whywhy/flow.py
@@ -1,31 +1,60 @@
 import os
 
-from malevich import flow, collection
+from malevich import flow, collection, run
 from malevich.interpreter.core import CoreInterpreter
 from malevich.whywhy import *
 
 sources_ =  "./data/rss_feed_sample.csv"
 recipients_ = "./data/email_workshop_test.csv"
-@flow(reverse_id='whywhy.newsletter.facts', name="Facts Newsletter")
+
+
+@flow(reverse_id="whywhy.feed.preload", name="Feed preloader")
+def feed_preloader(sources):
+    article_urls = run(get_article_url_from_rss(sources), alias="urls")
+    article_body = run(get_article_body_with_url(article_urls), alias="body")
+    return article_body
+
+
+@flow(reverse_id='whywhy.newsletter.facts.subflow', name="Facts Newsletter")
 def facts_newsletter():
     sources = collection(file=sources_, name="whywhy.sources")
     recipients = collection(file=recipients_, name="whywhy.recipients")
-    
-    article_urls = get_article_url_from_rss(sources)
-    article_body = get_article_body_with_url(article_urls)
 
-    sentences = split_article_to_sentence(article_body)
-    
-    fact_labels = sentence_classifier(sentences, config={"model": "fo", "subjectivity_threshold": 0.5})
-    sentiment_lables = sentence_classifier(sentences, config={"model": "sentiment", "sentiment_threshold": 0.5})
-    
-    summary = score_article(fact_labels, sentiment_lables)
+    article_body = feed_preloader(sources)
 
-    parsed_recipients = parse_recipeints(recipients)
+    sentences = run(
+        split_article_to_sentence(article_body),
+        alias="split"
+    )
+    
+    fact_labels = run(
+        sentence_classifier(
+            sentences, config={"model": "fo", "subjectivity_threshold": 0.5}
+        ), alias="fo"
+    )
+    sentiment_lables = run(
+        sentence_classifier(
+            sentences,
+            config={"model": "sentiment", "sentiment_threshold": 0.5}
+        ), alias="sentiment"
+    )
+    
+    summary = run(
+        score_article(fact_labels, sentiment_lables),
+        alias="score"
+    )
 
-    return send_newsletter(parsed_recipients, summary, config={"login": os.getenv("EMAIL_LOGIN"), "password": os.getenv("EMAIL_PASSWORD")})
+    parsed_recipients = run(parse_recipeints(recipients), alias="recep")
+
+    return run(
+        send_newsletter(
+            parsed_recipients, summary, 
+            config={"login": os.getenv("EMAIL_LOGIN"),
+                    "password": os.getenv("EMAIL_PASSWORD")}
+        ),
+        alias="send"
+    )
 
 
 if __name__ == "__main__":
     task = facts_newsletter()
-    task.interpret()
